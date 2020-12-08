dad99ceaae87974b3da69a480a6d3fc6e9f0170e
The Coding Buddha <thecodingbuddha@gmail.com>
2020-12-08 16:59:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# First.NetCoreMVCApplication
+ This repository for Tutorial First .Net Core MVC Application and this repository is designed for educational purposes. 
---
+++ SimpleDotNetCoreMVCApp/Models/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleDotNetCoreMVCApp.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ModelName { get; set; }
+        public decimal Price { get; set; }
+
+        static List<Car> carList = null;
+        static Car()
+        {
+            carList = new List<Car>(){
+                new Car()
+                {
+                    Id = 1,
+                    Name = "BMW",
+                    ModelName = "BMW S1",
+                    Price = 125000
+                }
+            };
+        }
+
+        public static List<Car> SelectCarList()
+        {
+            return carList;
+        }
+
+    }
+}
